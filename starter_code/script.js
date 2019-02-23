f1f45e7b89ae18a5861ac1b743cd1068b92e7e04
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-23 15:02:43 +0100

    background

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,22 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext('2d');    
+                                                                                                             
+function drawBackground() {
+
+}
+
+drawBackground()
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
+  
 
   function startGame() {
-
+    var image = new Image();
+    image.src = './images/bg.png';
+    ctx.drawImage(image, 0, 0, 1200, 600);
   }
 
 };
